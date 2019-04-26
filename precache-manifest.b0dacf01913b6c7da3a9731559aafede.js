72fa6acc7823c1fb079f5fb9bf20a7a683103f0c
Xurxe <xurxe.garcia@integrify.io>
2019-04-26 16:18:06 +0300

    Updates

---
+++ precache-manifest.b0dacf01913b6c7da3a9731559aafede.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb55191b04fbed8c6de6c9e8db0244c9",
+    "url": "/integrify-18-clone-react/index.html"
+  },
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "/integrify-18-clone-react/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "1618f2cb6dbddb6db86a",
+    "url": "/integrify-18-clone-react/static/js/runtime~main.266d5427.js"
+  }
+]);
