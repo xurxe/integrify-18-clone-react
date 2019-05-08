56099402f2bd938e2f7fb4bb8a0ec551703b6edf
Xurxe <xurxe.garcia@integrify.io>
2019-05-08 13:36:15 +0300

    Changed urls and added ids in data.js

--- src/data.js
+++ src/data.js
@@ -1,119 +1,127 @@
 let data = {
     header: {
+        id: 0,
         img: 'header-img.png',
         alt: '',
         title: '<em>Powerfully simple</em>&#8196;with a <br /> <em>squeeky clean</em>&#8196;design.',
         subtitle: 'Find out how you can offer quick and powerful <br /> solutions to your customers now!',
         button: {
+            id: 1,
             text: 'Learn more',
-            url: '/',
+            url: 'https://github.com/xurxe/integrify-18-clone-react',
         },
     },
 
     top: {
+        id: 2,
         title: 'Create a <em>powerful</em> solution now!',
         button: {
+            id: 3,
             text: 'Get started',
-            url: '/',
+            url: 'https://github.com/xurxe/integrify-18-clone-react',
         },
     },
 
     section1: {
+        id: 4,
         title: 'Features',
         content: [
             {
-                id: 0,
+                id: 5,
                 title: '<b>Superbly</b> responsive',
                 content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum, lorem at luctus placerat.',
                 img: '<i className="fas fa-mobile-alt"></i>',
                 button: {
+                    id: 6,
                     text: 'Read more',
-                    url: '/',
+                    url: 'https://github.com/xurxe/integrify-18-clone-react',
                 },
             },
             {
-                id: 1,
+                id: 7,
                 title: '<b>Squeeky</b> clean',
                 content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas suspendisse.',
                 img: '<i className="fas fa-desktop"></i>',
                 button: {
                     text: 'Read more',
-                    url: '/',
+                    url: 'https://github.com/xurxe/integrify-18-clone-react',
                 },
             },
             {
-                id: 2,
+                id: 8,
                 title: '<b>Multi</b> purpose',
                 content: 'Aenean at leo eros. Praesent pretium lacus at pellentesque eleifend. Quisque a felis scelerisque leo elementum.',
                 img: '<i className="fas fa-laptop"></i>',
                 button: {
                     text: 'Read more',
-                    url: '/',
+                    url: 'https://github.com/xurxe/integrify-18-clone-react',
                 },
             },
             {
-                id: 3,
+                id: 9,
                 title: '<b>Highly</b> flexible',
                 content: 'Ut ultrices turpis ut sem suscipit pellentesque. Aenean sed mollis felis. Suspendisse nunc nibh, vulputate id vulputate.',
                 img: '<i className="fas fa-recycle"></i>',
                 button: {
                     text: 'Read more',
-                    url: '/',
+                    url: 'https://github.com/xurxe/integrify-18-clone-react',
                 },
             },
         ],
     },
 
     section2: {
+        id: 10,
         title: 'Recent work',
         content: [
             {
-                id: 4,
+                id: 11,
                 title: 'Blogitty',
                 content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia volutpat hendrerit. Donec libero erat, cursus ut est quis, dictum...',
                 img: '',
                 alt: '',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 5,
+                id: 12,
                 title: 'Blogus',
                 content: 'Pellentesque nec turpis leo. Integer sit amet ante non lectus sollicitudin posuere. Sed commodo ut enim at scelerisque facilisi...',
                 img: '',
                 alt: '',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 6,
+                id: 13,
                 title: 'Blogarino',
                 content: 'Suspendisse fermentum molestie felis, a interdum est molestie ut. Curabitur ultricies ultrices elementum. Suspendisse potenti...',
                 img: '',
                 alt: '',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
         ],
     },
 
     section3: {
+        id: 14,
         title: 'Testimonials',
         content: [
             {
-                id: 7,
+                id: 15,
                 name: 'Jane Doe',
                 content: 'Praesent a iaculis nisi. Suspendisse non efficitur dui, sit amet pharetra mi. Etiam augue metus, dignissim ac egestas sed, gravida eu.',
             },
             {
-                id: 8,
+                id: 16,
                 name: 'Fulano de Tal',
                 content: 'Proin sagittis est augue, ut ornare orci varius sit amet. Suspendisse scelerisque lacinia cursus. Quisque nec vehicula ipsum.',
             },
             {
-                id: 9,
+                id: 17,
                 name: 'Maija Meikäläinen',
                 content: 'Aenean id nulla gravida, tincidunt turpis nec, fringilla felis. Aenean sed hendrerit libero, et ornare tellus. Quisque sed dui libero morbi non aenan scelerisque aliquet.',
             },
             {
-                id: 10,
+                id: 18,
                 name: 'Nisse i Hökarängen',
                 content: 'Sed eu aliquet lectus. Nunc sit amet est et lacus gravida faucibus interdum ac velit. Mauris et fermentum elit, et cursus metus.',
             },
@@ -121,17 +129,18 @@ let data = {
     },
 
     section4: {
+        id: 19,
         title: 'Development',
         content: [
             {
-                id: 11,
+                id: 20,
                 tab: 'Design',
                 class: 'active',
                 title: 'The Design',
                 content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde quibusdam veritatis expedita, eum temporibus inventore voluptatibus sint praesentium?</p> <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapiente eveniet nulla ullam, architecto sed laudantium facere officia magnam magni."</p>',
             },
             {
-                id: 12,
+                id: 21,
                 tab: 'Production',
                 class: '',
                 title: 'The Production',
@@ -141,107 +150,114 @@ let data = {
     },
 
     bottom: {
+        id: 22,
         title: 'Get in touch with us <em>now!</em>',
         button: {
+            id: 23,
             text: 'Contact us',
-            url: '/',
+            url: 'https://github.com/xurxe/integrify-18-clone-react',
         },
     },
 
     aboutUs: {
+        id: 24,
         title: 'About us',
         content: {
+            id: 25,
             description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores non praesentium quasi. Quae, quod.',
             address: '123 Street Name <br /> Road Name <br /> London <br /> +44 7419 13807'
         },
     },
 
     latestTweets: {
+        id: 25,
         title: 'Latest tweets',
         content: [
             {
-                id: 13,
+                id: 26,
                 username: '@Jainesh',
                 content: 'Just submitted another great item on <b>#themeforest</b>',
                 timestamp: '3 hours ago',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 14,
+                id: 27,
                 username: '@Jainesh',
                 content: 'Working on an awesome theme for <b>#themeforest</b>',
                 timestamp: '3 weeks ago',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
         ],
     },
 
     latestPosts: {
+        id: 28,
         title: 'Latest posts',
         content: [
             {
-                id: 15,
+                id: 29,
                 title: 'Lorem ipsum dolor sit amet, consect',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 16,
+                id: 30,
                 title: 'Sea do elusmad tempor incididunt!',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 17,
+                id: 31,
                 title: 'Ut labore et dolare magnar sea do elusmad tempor',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
             {
-                id: 18,
+                id: 32,
                 title: 'Consectetur incididunt elt, sea do?',
-                url: '/',
+                url: 'https://github.com/xurxe/integrify-18-clone-react',
             },
         ],
     },
     
     flickr: {
+        id: 33,
         title: 'Flickr',
         content: [
             {
-                id: 19,
+                id: 34,
                 img: '',
                 alt: '',
             },
             {
-                id: 20,
+                id: 35,
                 img: '',
                 alt: '',
             },
             {
-                id: 21,
+                id: 36,
                 img: '',
                 alt: '',
             },
             {
-                id: 22,
+                id: 37,
                 img: '',
                 alt: '',
             },
             {
-                id: 23,
+                id: 38,
                 img: '',
                 alt: '',
             },
             {
-                id: 24,
+                id: 39,
                 img: '',
                 alt: '',
             },
             {
-                id: 25,
+                id: 40,
                 img: '',
                 alt: '',
             },
             {
-                id: 26,
+                id: 41,
                 img: '',
                 alt: '',
             },
@@ -249,40 +265,41 @@ let data = {
     },
 
     footer: {
+        id: 42,
         copyright: 'Copyright &copy; 2003-2012 Ventus Inc. All rights reserved.',
         socialMedia: [
             {
-                id: 27,
+                id: 43,
                 name: 'Twitter',
                 img: '<i className="fab fa-twitter-square"></i>',
                 url: 'https://twitter.com/'
             },
             {
-                id: 28,
+                id: 44,
                 name: 'Facebook',
                 img: '<i className="fab fa-facebook-square"></i>',
                 url: 'https://facebook.com/'
             },
             {
-                id: 29,
+                id: 45,
                 name: 'Vimeo',
                 img: '<i className="fab fa-vimeo-square"></i>',
                 url: 'https://vimeo.com/'
             },
             {
-                id: 30,
+                id: 46,
                 name: 'YouTube',
                 img: '<i className="fab fa-youtube-square"></i>',
                 url: 'https://youtube.com/'
             },
             {
-                id: 31,
+                id: 47,
                 name: 'Google+',
                 img: '<i className="fab fa-google-plus-square"></i>',
                 url: 'https://plus.google.com/'
             },
             {
-                id: 32,
+                id: 48,
                 name: 'Pinterest',
                 img: '<i className="fab fa-pinterest-square"></i>',
                 url: 'https://pinterest.com/'
